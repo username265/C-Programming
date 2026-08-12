43777a20e957020ffd7ef90a1af6f20e0c309030
y <identify562@gmail.com>
2026-08-12 21:59:15 +0900

    arithmetic-operators.c

---
+++ 03-Operators-and-Binary/01-arithmetic-operators.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a = 10;
+    int b = 3;
+
+    printf("a + b = %d\n", a + b);
+    printf("a - b = %d\n", a - b);
+    printf("a * b = %d\n", a * b);
+    printf("a / b = %d\n", a / b);
+    printf("a %% b = %d\n", a % b);
+
+    return 0;
+}
