71aae3865b307aaac07f5d0ecdcd883f427872f3
y <identify562@gmail.com>
2026-08-17 11:04:56 +0900

    Create 01-scanf-basic.c

---
+++ 04-Input/01-scanf-basic.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int number;
+
+    printf("Enter an integer: ");
+    scanf("%d", &number);
+
+    printf("You entered: %d\n", number);
+
+    return 0;
+}
