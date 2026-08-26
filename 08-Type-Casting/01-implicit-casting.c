d93180a21a70f1b4bc7f72f00921c8f4094b9ad4
y <identify562@gmail.com>
2026-08-26 15:43:20 +0900

    Create 01-implicit-casting.c

---
+++ 08-Type-Casting/01-implicit-casting.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a;
+    double b = 2.4;
+
+    a = b;
+
+    printf("double value = %f\n", b);
+    printf("int value    = %d\n", a);
+
+    return 0;
+}
